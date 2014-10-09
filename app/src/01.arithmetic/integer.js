a5c91ff300d68970eabdbfa2d98532ea908f26ba
evan <evan.liomain@gmail.com>
2014-10-09 15:48:37 +0200

    Refacto the file structure and test code to be more logical. Add exercice and test : isPrime, module, quotient, array.length and array.isEmpty.

---
+++ app/src/01.arithmetic/integer.js
@@ -0,0 +1,64 @@
+define(function() {
+  'use strict';
+  return {
+    /**
+     * test if n is even
+     * Code it with just 1 ligne of code
+     * @param  {integer}  n
+     * @return {Boolean}   true if n is even, else false
+     */
+    isEven : function(n) {
+      // TODO
+      return null;
+    },
+    /**
+     * test if n is even
+     * Code it with no euclidian operator
+     * @param  {integer}  n
+     * @return {Boolean}   true if n is even, else false
+     */
+    isEvenBasic : function(n) {
+      // TODO
+      return null;
+    },
+    /**
+     * Get the integer n^m, or n*n*n*...*n (m times)
+     * @param  {integer} n the indice
+     * @param  {integer} m the exposant
+     * @return {integer} n^m
+     */
+    pow : function(n, m) {
+      // TODO
+      return null;
+    },
+    /**
+     * Get the module in the euclidean division a/b
+     * @param  {integer} a
+     * @param  {integer} b divisor
+     * @return {integer} return r as it exist q as a = qb + r
+     */
+    module : function(a, b) {
+      // TODO
+      return null;
+    },
+    /**
+     * Get the quotient in the euclidean division a/b
+     * @param  {integer} a
+     * @param  {integer} b divisor
+     * @return {integer} return q as it exist r as a = qb + r
+     */
+    quotient : function(a, b) {
+      // TODO
+      return null;
+    },
+    /**
+     * Test if n is prime
+     * @param  {integer}  n
+     * @return {Boolean}  true if n is prime, false else
+     */
+    isPrime : function(n) {
+      // TODO
+      return null;
+    }
+  };
+});
---
+++ app/src/02.structure/array.js
@@ -0,0 +1,21 @@
+define(function() {
+  'use strict';
+  return {
+    /**
+     * Get the length of an array
+     * @param  {Array} a
+     * @return {integer} the item count of a
+     */
+    length : function(a) {
+      return null;
+    },
+    /**
+     * Test if an anrray is empty
+     * @param  {Array} a
+     * @return {Boolean} true if a is empty, false else
+     */
+    isEmpty : function(a) {
+      return null;
+    },
+  };
+});
